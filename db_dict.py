b168bd4c80475923919281a6607430de3b4d9144
PlushkaNET <.>
2026-01-26 00:56:18 +0300

    serializer&desializer added

--- db_dict.py
+++ db_dict.py
@@ -2,13 +2,38 @@
 This is the file contains code of the DBDict
 DBDict is alternative user-friendly method for using dict's,
 but without saving values and keys to memory!
-In future there will be json serializer and deserializer.
-Now it's supports only strings in keys and values but in future it's gonna change.
+Now it's supports only list, dict, int, float and bool types in values,
+and only strings in keys, but in future it's gonna change.
 
 Currently only supports SQLite
 """
 
 from sqlite3 import connect
+import json
+
+"""
+There are two types of errors.
+First (UnserializableError) is for objects that cannot be serialized
+(like dicts/lists with classes/functions).
+And second (UnsupportedType) is for directly passed objects/functions (unzerializble objects)
+"""
+
+class UnserializableError(Exception):
+    def __init__(self, text):
+        super().__init__(text)
+
+class UnsupportedType(Exception):
+    def __init__(self, text):
+        super().__init__(text)
+
+"""
+Types codes:
+0 - str (original)
+1 - json
+2 - int
+3 - float
+4 - bool
+"""
 
 class DBDict:
     def __setup__(self):
@@ -21,7 +46,7 @@ class DBDict:
             data = cursor.fetchone()
             if not data:
                 cursor.execute("INSERT INTO dicts_info VALUES (?)", (self.name,))
-                cursor.execute(f"CREATE TABLE {self.name} (D_KEY TEXT PRIMARY KEY, D_VALUE TEXT)")
+                cursor.execute(f"CREATE TABLE {self.name} (D_KEY TEXT PRIMARY KEY, D_VALUE TEXT, VALUE_TYPE INT)")
             cursor.close()
             db.commit()
 
@@ -32,19 +57,46 @@ class DBDict:
         self.path = path
         self.__setup__()
 
+    def __convert_(self, obj) -> tuple[str, int]:
+        obj_type = type(obj)
+        if obj_type == str:
+            return obj, 0
+        if obj_type == dict or obj_type == list:
+            try:
+                serialized = json.dumps(obj)
+            except:
+                raise UnserializableError("Object cannot be serialized, because it probably contains unusual data (like classes/functions)")
+            else:
+                return serialized, 1
+        if obj_type == int:
+            return str(obj), 2
+        if obj_type == float:
+            return str(obj), 3
+        if obj_type == bool:
+            return str(int(obj)), 4
+        raise UnsupportedType("Object is not supported type")
+        
+    def __deconvert_(self, obj:str, otype):
+        if otype == 0: return obj
+        if otype == 1: return json.loads(obj)
+        if otype == 2: return int(obj)
+        if otype == 3: return float(obj)
+        if otype == 4: return bool(int(obj))
+
     def __setitem__(self, key, value):
         with connect(self.path) as db:
             # types of key and value MUST be str, otherwise it will throw an error
-            db.execute(f"INSERT INTO {self.name} VALUES (?, ?) ON CONFLICT (D_KEY) DO UPDATE SET D_VALUE = EXCLUDED.D_VALUE", (key, value,))
+            value, vtype = self.__convert_(value)
+            db.execute(f"INSERT INTO {self.name} VALUES (?, ?, ?) ON CONFLICT (D_KEY) DO UPDATE SET D_VALUE = EXCLUDED.D_VALUE, VALUE_TYPE = EXCLUDED.VALUE_TYPE", (key, value, vtype,))
             db.commit()
     
     def __getitem__(self, key):
         with connect(self.path) as db:
             # type of key MUST be str, otherwise it will throw an error
-            cursor = db.execute(f"SELECT D_VALUE FROM {self.name} WHERE D_KEY = ?", (key,))
+            cursor = db.execute(f"SELECT D_VALUE, VALUE_TYPE FROM {self.name} WHERE D_KEY = ?", (key,))
             value = cursor.fetchone()
             if value:
-                value = value[0]
+                value = self.__deconvert_(value[0], value[1])
             else:
                 value = None
             cursor.close()
@@ -63,18 +115,20 @@ class DBDict:
         with connect(self.path) as db:
             db.execute(f"DELETE FROM {self.name} WHERE D_KEY = ?", (key,))
             db.commit()
-    
-    def __generator_(self, value:str, unpack):
+
+    def __fetch_values_(self, values:str):
         with connect(self.path) as db:
-            cursor = db.execute(f"SELECT {value} FROM {self.name}")
+            cursor = db.execute(f"SELECT {values}, VALUE_TYPE FROM {self.name}")
             data = cursor.fetchall()
             cursor.close()
-        
-        if unpack:
-            yield from [i[0] for i in data]
-        else:
-            yield from data
+        return data
     
-    def keys(self): return self.__generator_("D_KEY", True)
-    def values(self): return self.__generator_("D_VALUE", True)
-    def items(self): return self.__generator_("D_KEY, D_VALUE", False)
+    def __generator_(self, values:str):
+        data = self.__fetch_values_(values)
+        yield from [self.__deconvert_(i[0], i[1]) for i in data]
+    
+    def keys(self): return self.__generator_("D_KEY")
+    def values(self): return self.__generator_("D_VALUE")
+    def items(self):
+        data = self.__fetch_values_("D_KEY, D_VALUE")
+        yield from [(i[0], self.__deconvert_(i[1], i[2])) for i in data]
--- example.py
+++ example.py
@@ -5,6 +5,12 @@ example_dict = DBDict("db_example.db", "example")
 
 example_dict["key1"] = "content here 1"
 example_dict["key2"] = "content here 2"
+example_dict["key3"] = True
+example_dict["key4"] = False
+example_dict["key5"] = 15
+example_dict["key6"] = 91.8
+example_dict["key7"] = [8, "hello"]
+example_dict["key8"] = {"something":"something"}
 
 for i in example_dict.items():
     print(f"Key: {i[0]}\tValue: {i[1]}")
@@ -17,3 +23,9 @@ else:
     print("It not exists!")
 
 print(example_dict["key2"])
+print(example_dict["key3"])
+print(example_dict["key4"])
+print(example_dict["key5"])
+print(example_dict["key6"])
+print(example_dict["key7"])
+print(example_dict["key8"])
