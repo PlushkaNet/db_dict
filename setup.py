5774991bf09c7bf51ceb4b3a5f47bfa418890b03
PlushkaNET <.>
2026-03-05 21:17:51 +0300

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="db_dict",
-    version="0.31",
+    version="0.32",
     description="Easy to use user-friendly sqlite db interface",
     packages=find_packages(),
     requires=[
